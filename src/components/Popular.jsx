49d4f892c63a61d24b67cfe86b9cc4db09b3ad4c
imskanand <the.anand3099@gmail.com>
2022-04-28 18:44:37 +0530

    API Call to the food API

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from 'react';
+import style from 'styled-components';
+
 function Popular() {
+
+  const [popular, setPopular] = useState([])
+
+  // Run the getPopular function when the component get mounted
+  useEffect(() => { 
+    getPopular()
+  },[])
+
+  const getPopular = async() =>{
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+    const data = await api.json()
+    setPopular(data.recipes)
+  }
+  
   return (
-    <div>Popular</div>
+    <div>
+      { popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
